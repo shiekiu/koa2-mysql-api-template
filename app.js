bd8b3e997f4d0ff972a5024de5989753eda05752
shiekiu <shiekiu@sina.com>
2018-01-25 09:43:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,21 @@ app.use(async (ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`)
     await next()
 })
-// 了一个解析HTTP请求body的处理函数。如果HTTP请求是JSON数据，我们就可以通过ctx.request.body直接访问解析后的JavaScript对象。
-app.use(bodyParser())
+// 返回客户端401错误
+app.use(function(ctx, next){
+    return next().catch((err) => {
+      if (401 == err.status) {
+        ctx.status = 401;
+        ctx.body = '{ statuscode: 401, message: "无访问权限"}';
+      } else {
+        throw err;
+      }
+    })
+})
 //数组中的路径不需要通过jwt验证，除了api/login都要验证token
 app.use(jwtKoa({secret: secret.sign}).unless({path: [/^\/api\/login/, /^\/api\/createUser/]}))
+// 了一个解析HTTP请求body的处理函数。如果HTTP请求是JSON数据，我们就可以通过ctx.request.body直接访问解析后的JavaScript对象。
+app.use(bodyParser())
 app.use(rest.restify())
 app.use(controller())
 app.listen(3001);
